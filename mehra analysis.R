facbaf0aa9c1d4432ae78f8a553b230832a9753b
Tjebbe <tjebbe.bodewes@live.nl>
2018-07-26 13:43:13 +0100

    Started building MEHRA analysis workflow

--- mehra analysis.R
+++ mehra analysis.R
@@ -2,31 +2,29 @@ library(data.table)
 library(tidyverse)
 library(GGally)
 
-invisible(lapply(c("Scoring.R", "Fit DAG.R", "Experiments.R"), source))
+invisible(lapply(c("Scoring.R", "Fit DAG.R", "Experiments.R", 
+                   "MEHRA functions"), source))
 
-testing <- readRDS("C:/Users/tjbodewe/Desktop/Data/testing.rds")
-training <- readRDS("C:/Users/tjbodewe/Desktop/Data/training.rds")
-mehra <- rbindlist(list(training, testing))
-rm(testing, training)
-gc()
+#' TODO:
+#' - Document computeMSE and parImpute
+#' - Test computeMSE
+#' - Test and benchmark parImpute
 
-mehra.daily <- mehra %>% select(-Hour) %>%
-  group_by(Region, Zone, Type, Year, Month, Day, Latitude, Longitude, Altitude, Season) %>%
-  summarise_all(mean)
+#' ** Structure learning and hyperparameter tuning **
+mehra.daily <- readRDS("mehra_daily.RDS")
 
-mehra.daily <- mehra.daily %>% 
-  group_by(Region, Zone, Type, Latitude, Longitude, Altitude) %>%
-  mutate(Year = as.numeric(Year) + 1980) %>%
-  mutate(CVD.ly = lead(CVD60, 365)) %>%
-  select(Region, Zone, Type, Year, Month, Day, Season, everything()) %>%
-  as.data.frame()
+training <- filter(mehra.daily, Year <= 2004)
+validation <- filter(mehra.daily, between(Year, 2005, 2006))
+testing <- filter(mehra.daily, Year > 2006)
 
-saveRDS(mehra.daily, file = "MEHRA_daily.RDS")
-rm(mehra) 
-gc()
+penalties <- c("0.10", "0.25")
+thresholds <- c(0.50, 0.75, 0.95, 0.99)
+no.boot.tuning <- 100
+no.boot.testing <- 200
+particles.tuning <- 100
+particles.testing <- 100
 
-
-bl = data.frame(
+bl <-  {data.frame(
   "from" = c(rep("Region", 8), rep("Zone", 8), rep("Type", 8), rep("Year", 8),
              rep("Season", 8), rep("Month", 8),
              rep("Latitude", 8), rep("Longitude", 8), rep("Altitude", 8),
@@ -79,19 +77,54 @@ bl = data.frame(
            "Year", "Season", "Month", "Latitude", "Longitude",
            "Altitude", "t2m", "ws", "wd", "tp", "blh", "ssr", "no2", "o3",
            "so2", "co", "pm10", "pm2.5", "CVD60", "CVD.lm", "CVD.ly"))
+}
+
+bootSamples10 <- bootstrap.dag()
+
+
+#' ** Data extraction and transformation **
+testing <- readRDS("C:/Users/tjbodewe/Desktop/Data/testing.rds")
+training <- readRDS("C:/Users/tjbodewe/Desktop/Data/training.rds")
+mehra <- rbindlist(list(training, testing))
+rm(testing, training)
+gc()
+
+mehra.daily <- mehra %>% select(-Hour) %>%
+  group_by(Region, Zone, Type, Year, Month, Day, Latitude, Longitude, Altitude, Season) %>%
+  summarise_all(funs(mean), na.rm = TRUE)
+
+mehra.daily <- mehra.daily %>% 
+  group_by(Region, Zone, Type, Latitude, Longitude, Altitude) %>%
+  mutate(Year = as.numeric(Year) + 1980) %>%
+  mutate(CVD.ly = lead(CVD60, 365)) %>%
+  select(Region, Zone, Type, Year, Month, Day, Season, everything()) %>%
+  as.data.frame()
+
+saveRDS(mehra.daily, file = "MEHRA_daily.RDS")
+rm(mehra) 
+gc()
+
+
+
 mehra.daily <- readRDS("mehra_daily.RDS")
 
 training <- filter(mehra.daily, Year <= 2006)
-testing <- filter(mehra.daily, Year > 2006)
+validation <- filter(mehra.daily, between(Year, 2007, 2010))
+testing <- filter(mehra.daily, Year > 2010)
+
+head(mehra.daily)
+lapply(training, function(x) mean(is.na(x))) #Over 99% missing for pm2.5!
+lapply(validation, function(x) mean(is.na(x)))
+lapply(testing, function(x) mean(is.na(x)))
 
 mehra.bootsamples <- bootstrap.dag(training, penalty = "0.25", blacklist = bl, samples = 200) 
 saveRDS(mehra.bootsamples, "MEHRA_bootsamples.RDS")
 
 mehra.bootsamples <- readRDS("MEHRA_bootsamples.RDS")
 
-strength <- custom.strength(mehra.bootsamples, names(mehra.daily))
+strength <- custom.strength(mehra.bootsamples, c(names(mehra.daily), "CVD.lm", "CVD.l1000"))
 
-dag.mehra.99 <- averaged.network(strength, names(mehra.daily), 0.99)
+dag.mehra.99 <- averaged.network(strength, c(names(mehra.daily), "CVD.lm", "CVD.l1000"), 0.99)
 
 dag.mehra.95 <- averaged.network(strength, names(mehra.daily), 0.95)
 
@@ -100,16 +133,48 @@ dag.mehra.99 <- set.arc(dag.mehra.99, "Month", "pm2.5")
 dag.mehra.95 <- set.arc(dag.mehra.95, "Month", "pm2.5")
 
 pdf("MEHRA_dag99.pdf", width = 10, height = 10)
-graphviz.plot(subgraph(dag.mehra.99, names(mehra.daily)[-c(24, 26)]))
+graphviz.plot(subgraph(dag.mehra.99, names(mehra.daily)))
 dev.off()
 
 pdf("MEHRA_dag95.pdf", width = 15, height = 15)
-graphviz.plot(subgraph(dag.mehra.95, names(mehra.daily)[-c(24, 26)]))
+graphviz.plot(subgraph(dag.mehra.95, names(mehra.daily)))
 dev.off()
 
-mehra.fit <- bn.fit(dag.mehra.99, mehra.daily)
+mehra.fit <- bn.fit(dag.mehra.99, training)
+mehra.em <- em.parametric(dag.mehra.99, sample_n(training, 10000), max.iter = 1, debug = TRUE)$dag
 mehra.fit[[15]]
 
+dag.test <- fit.dag(mehra.daily, "0.25")
+
+
+RMSE.train <- sapply(names(mehra.daily)[8:24], computeRMSE, 
+                     dat = training, dag.fit = mehra.fit, train = TRUE)
+
+
+cl <- makeCluster(detectCores() - 1)
+invisible(clusterEvalQ(cl, library(bnlearn)))
+
+RMSE.testing <- parSapply(cl, names(mehra.daily)[8:24], computeRMSE, 
+                       dat = sample_n(testing, 1000), dag.fit = mehra.em, train = FALSE,
+                       impute = TRUE)
+stopCluster(cl)
+RMSE.testing
+
+BN <- mehra.fit
+col2remove <- c()
+for (nCol in names(which(sapply(testing, class) == 'factor'))){
+  stringCol <- paste("BN$", nCol, "$prob", sep = "")
+  levelsCol <- names(which( eval(parse(text = stringCol)) != 0 ))
+  if (!all(unique(testing[,nCol]) %in% levelsCol)) {
+    col2remove <- c(col2remove, nCol)
+  }
+}
+col2remove <- which(names(testing) %in% col2remove)
+
+#Core of problem: combinations of factors and missing values that don't 
+#occur in the training set but do occur in the testing set
+with(training, sum(!is.na(pm10) & !is.na(pm2.5) & Region == "East Midlands"))
+with(testing, mean(!is.na(pm10) & !is.na(pm2.5) &  Region == "East Midlands"))
 
 #' ** EDA **
 #'
